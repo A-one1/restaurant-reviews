27650cda97f4346d5ef7e248de37cc03faf953b3
Aone <aoneshrestha58@gmail.com>
2023-05-24 06:23:29 -0500

    routes added

--- backend/dao/restaurantsDAO.js
+++ backend/dao/restaurantsDAO.js
@@ -1,5 +1,6 @@
 import mongodb from "mongodb"
-const ObjectId = mongodb.ObjectID
+
+const ObjectId = mongodb.ObjectId
 
 let restaurants
 
@@ -57,60 +58,60 @@ export default class RestaurantsDAO {
       return { restaurantsList: [], totalNumRestaurants: 0 }
     }
   }
-  // static async getRestaurantByID(id) {
-  //   try {
-  //     const pipeline = [
-  //       {
-  //           $match: {
-  //               _id: new ObjectId(id),
-  //           },
-  //       },
-  //             {
-  //                 $lookup: {
-  //                     from: "reviews",
-  //                     let: {
-  //                         id: "$_id",
-  //                     },
-  //                     pipeline: [
-  //                         {
-  //                             $match: {
-  //                                 $expr: {
-  //                                     $eq: ["$restaurant_id", "$$id"],
-  //                                 },
-  //                             },
-  //                         },
-  //                         {
-  //                             $sort: {
-  //                                 date: -1,
-  //                             },
-  //                         },
-  //                     ],
-  //                     as: "reviews",
-  //                 },
-  //             },
-  //             {
-  //                 $addFields: {
-  //                     reviews: "$reviews",
-  //                 },
-  //             },
-  //         ]
-  //     return await restaurants.aggregate(pipeline).next()
-  //   } catch (e) {
-  //     console.error(`Something went wrong in getRestaurantByID: ${e}`)
-  //     throw e
-  //   }
-  // }
+  static async getRestaurantByID(id) {
+    try {
+      const pipeline = [
+        {
+            $match: {
+                _id: new ObjectId(id),
+            },
+        },
+              {
+                  $lookup: {
+                      from: "reviews",
+                      let: {
+                          id: "$_id",
+                      },
+                      pipeline: [
+                          {
+                              $match: {
+                                  $expr: {
+                                      $eq: ["$restaurant_id", "$$id"],
+                                  },
+                              },
+                          },
+                          {
+                              $sort: {
+                                  date: -1,
+                              },
+                          },
+                      ],
+                      as: "reviews",
+                  },
+              },
+              {
+                  $addFields: {
+                      reviews: "$reviews",
+                  },
+              },
+          ]
+      return await restaurants.aggregate(pipeline).next()
+    } catch (e) {
+      console.error(`Something went wrong in getRestaurantByID: ${e}`)
+      throw e
+    }
+  }
 
-  // static async getCuisines() {
-  //   let cuisines = []
-  //   try {
-  //     cuisines = await restaurants.distinct("cuisine")
-  //     return cuisines
-  //   } catch (e) {
-  //     console.error(`Unable to get cuisines, ${e}`)
-  //     return cuisines
-  //   }
-  // }
+  static async getCuisines() {
+    let cuisines = []
+    try {
+      cuisines = await restaurants.distinct("cuisine")
+      return cuisines
+    } catch (e) {
+      console.error(`Unable to get cuisines, ${e}`)
+      return cuisines
+    }
+  }
 }
 
 
