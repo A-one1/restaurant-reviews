d1c3cef97ac3c23220db21e8254fd47e7fbfa446
Aone <aoneshrestha58@gmail.com>
2023-05-26 05:54:44 -0500

    added map

--- frontend/src/components/restaurants.js
+++ frontend/src/components/restaurants.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import {Switch, Route, Link} from "react-router-dom";
 
-function Restaurant() {
+const Restaurant = props => {
+  const initialRestaurantState = {
+    id:null,
+    name:"",
+    address:{},
+    cuisine:"",
+    reviews:[]
+  }
+const [restaurant,setRestaurant] = useState(initialRestaurantState);
+
+const getRestaurant = id =>{
+  Restaurant.
+}
+
+  
+
   return (
     <div className="App">
       Hello Worldssss
